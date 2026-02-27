6b3eb93f4a72d79036cb3722b6c5449c5ce352cd
Azra Akgün <akgunazra2004@gmail.com>
2026-02-27 16:51:38 +0300

    Add files via upload

---
+++ myFirstLabScript/main.py
@@ -0,0 +1,14 @@
+
+
+def print_hi(greetings):
+
+    print('What is your name?')
+    print('Azra')
+    print('Hello Azra')
+    print('What is your Student ID?')
+    print('20190120023')
+    print('Your ID İS 20190120023')
+
+
+
+
---
+++ myFirstLabScript/timed_logic.py
@@ -0,0 +1,11 @@
+second=int (input("enter a large integer"))
+hour=second //3600
+remain=second %3600
+minute=remain //60
+minute=remain %60
+
+print(f"{second} second, {hour} hour,{minute} minute,{second} second")
+
+
+
+
